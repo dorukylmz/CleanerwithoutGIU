9a291e1c45aac5f76f256246b62dee3d1f4dd145
dorukylmz <93840231+dorukylmz@users.noreply.github.com>
2022-04-06 13:14:32 +0300

    Add files via upload

--- cleaner.py
+++ cleaner.py
@@ -1,20 +1,20 @@
-from ast import While
 import glob
 import sys
 import os
 import time
 import datetime
-from apscheduler.schedulers.blocking import BlockingScheduler
 
-
-address=sys.argv[1]
-days=sys.argv[2]
-files=glob.glob(address+"/*")
-#while True:
-for x in files:
-        lastmod=time.ctime(os.path.getmtime(x))
-        lastmod=datetime.datetime.strptime(lastmod, "%a %b  %d %X %Y")
-
-        print(lastmod)
-        end_date = lastmod + datetime.timedelta(days=10)
-        print(end_date)
+try:
+        address=sys.argv[1]
+        day=sys.argv[2]
+        files=glob.glob(address+"/*")
+        for x in files:
+                lastmod=time.ctime(os.path.getmtime(x))
+                lastmod=datetime.datetime.strptime(lastmod, "%a %b  %d %X %Y")
+                if lastmod< datetime.datetime.now()-datetime.timedelta(days=int(day)):
+                        print(f'{x} will be removed')
+                        os.remove(x)
+                else :
+                        pass
+except:
+        print("Parametre hatası")
